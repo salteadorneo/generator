410a1aca34e345f3f9bb8c5f71356f63e1661796
Cristian Adán <hola@salteadorneo.dev>
2024-10-03 02:37:55 +0200

    Refactor module imports and paths for better code organization

---
+++ src/i18n/ui.ts
@@ -0,0 +1,17 @@
+export const showDefaultLang = false;
+
+export const languages = {
+    es: 'Español',
+    en: 'English',
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+    es: {
+        'hero.description': 'Genere datos aleatorios para sus proyectos. Elija el tipo de datos que desea generar y la cantidad de datos que necesita.',
+    },
+    en: {
+        'hero.description': 'Generate random data for your projects. Choose the type of data you want to generate and the amount of data you need.',
+    },
+} as const;
