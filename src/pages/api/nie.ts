80e28d5f78e7ca3241a48a61ad1590b329e817a0
Cristian Adán <salteadorneo@users.noreply.github.com>
2024-10-03 12:28:57 +0000

    Create API routes to generate random data

---
+++ src/pages/api/nie.ts
@@ -0,0 +1,21 @@
+import { generateNIE } from '@utils/nif';
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = ({ request }) => {
+    const searchParams = new URL(request.url);
+    const count = searchParams.searchParams.get('count') || '1';
+
+    const data = [];
+    for (let i = 0; i < parseInt(count); i++) {
+        data.push(generateNIE());
+    }
+
+    return new Response(
+        JSON.stringify(data),
+        {
+            headers: {
+                'content-type': 'application/json',
+            },
+        },
+    )
+}
---
+++ src/pages/api/password.ts
@@ -0,0 +1,21 @@
+import { generatePassword } from '@utils/password';
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = ({ request }) => {
+    const searchParams = new URL(request.url);
+    const count = searchParams.searchParams.get('count') || '1';
+
+    const data = [];
+    for (let i = 0; i < parseInt(count); i++) {
+        data.push(generatePassword());
+    }
+
+    return new Response(
+        JSON.stringify(data),
+        {
+            headers: {
+                'content-type': 'application/json',
+            },
+        },
+    )
+}
