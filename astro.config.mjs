a816b0bbff16099eb2f8a154b6a76919907cc900
Cristian Adán <salteadorneo@users.noreply.github.com>
2024-10-03 12:28:40 +0000

    Use Deno adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-
+import deno from '@deno/astro-adapter'
 import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
-  site: 'https://generator.salteadorneo.dev',
+  output: 'server',
+  adapter: deno(),
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en']
