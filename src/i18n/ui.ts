40d235e017bd5fb1d4cdd9ba9a84c18ddab9e810
Cristian Adán <hola@salteadorneo.dev>
2024-10-07 19:15:41 +0200

    Refactor UI translations for hero description

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -9,9 +9,9 @@ export const defaultLang = 'es';
 
 export const ui = {
     es: {
-        'hero.description': 'Genere datos aleatorios para sus proyectos. Elija el tipo de datos que desea generar y la cantidad de datos que necesita.',
+        'hero.description': 'Genere datos aleatorios para sus proyectos.',
     },
     en: {
-        'hero.description': 'Generate random data for your projects. Choose the type of data you want to generate and the amount of data you need.',
+        'hero.description': 'Generate random data for your projects.',
     },
 } as const;
