59a9ffa3ccca75884384e0668b4b8368ea7a7d01
Cristian Adán <hola@salteadorneo.dev>
2024-10-08 19:38:14 +0200

    Refactor password API to accept custom length parameter

--- src/pages/api/v1/password.ts
+++ src/pages/api/v1/password.ts
@@ -4,9 +4,10 @@ import type { APIRoute } from 'astro';
 export const GET: APIRoute = ({ request }) => {
     const searchParams = new URL(request.url);
     const quantity = searchParams.searchParams.get('quantity') || '1';
+    const length = searchParams.searchParams.get('length') || '16';
 
     return new Response(
-        JSON.stringify(Array.from({ length: parseInt(quantity) }, () => generatePassword())),
+        JSON.stringify(Array.from({ length: parseInt(quantity) }, () => generatePassword(parseInt(length)))),
         {
             headers: {
                 'content-type': 'application/json',
