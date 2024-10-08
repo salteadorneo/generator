9ec353188c9c70c8f12ece19afb414c2e1961bf6
Cristian Adán <hola@salteadorneo.dev>
2024-10-08 20:40:53 +0200

    Refactor card types in card.ts

--- src/utils/card.ts
+++ src/utils/card.ts
@@ -1,8 +1,16 @@
 export const cardTypes = [
-    { name: 'Visa', prefix: ['4'], length: 16, cvvLength: 3 },
-    { name: 'MasterCard', prefix: ['51', '52', '53', '54', '55'], length: 16, cvvLength: 3 },
     { name: 'American Express', prefix: ['34', '37'], length: 15, cvvLength: 4 },
-    { name: 'Discover', prefix: ['6011', '65'], length: 16, cvvLength: 3 },
+    { name: 'Carte Blanche', prefix: ['38'], length: 14, cvvLength: 3 },
+    { name: 'Diners Club', prefix: ['300', '301', '302', '303', '36', '38'], length: 14, cvvLength: 3 },
+    { name: 'Discover', prefix: ['6011', '622', '64', '65'], length: 16, cvvLength: 3 },
+    { name: 'Enroute', prefix: ['2014', '2149'], length: 15, cvvLength: 3 },
+    { name: 'JCB', prefix: ['35'], length: 16, cvvLength: 3 },
+    { name: 'Maestro', prefix: ['5018', '5020', '5038', '6304', '6759', '6761', '6762', '6763'], length: 16, cvvLength: 3 },
+    { name: 'MasterCard', prefix: ['51', '52', '53', '54', '55'], length: 16, cvvLength: 3 },
+    { name: 'Solo', prefix: ['6334', '6767'], length: 16, cvvLength: 3 },
+    { name: 'Switch', prefix: ['4903', '4905', '4911', '4936', '564182', '633110', '6333', '6759'], length: 16, cvvLength: 3 },
+    { name: 'Visa', prefix: ['4'], length: 16, cvvLength: 3 },
+    { name: 'Visa Electron', prefix: ['4026', '417500', '4508', '4844', '4913', '4917'], length: 16, cvvLength: 3 },
 ];
 
 function luhnAlgorithm(cardNumber: string): boolean {
